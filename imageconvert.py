9715ee41dbf6d9f97e8e754b5818212453b46b9b
Nathan <nathanbooth@Nathans-MacBook-Air.local>
2015-09-02 13:10:40 -0500

    added aspect ratio

--- imageconvert.py
+++ imageconvert.py
@@ -4,35 +4,42 @@ import os
 import sys
 import getopt
 import argparse
+import PIL
 from PIL import Image
-
+import numpy as np
 
 # command line functions
 
 
 
 def Command():
-    opts, args = getopt.getopt(sys.argv[1:], 'd:w:h:')
+    opts, args = getopt.getopt(sys.argv[1:], 'd:bw:bh:')
     directory = ''
-    width = -1
-    height = -1
+    basewidth = -1
+    baseheight = 1200
     
     for opt, arg in opts:
 	if opt == '-d':
 	    directory = arg
-	elif opt == '-w':
+	elif opt == '-bw':
 	    width = int(arg)
-	elif opt == '-h':
+	elif opt == '-bh':
 	    height = int(arg)
     
     for image in os.listdir(directory):
 	print('Resizing image' + image)
+        img = Image.open(os.path.join(directory, image))
+	#img.load()
+	#image_data = np.asarray(img)
 
-	img = Image.open(os.path.join(directory, image))
 
-	img = img.resize((width, height), Image.BILINEAR)
+	wpercent = (baseheight/float(img.size[0]))
+	hsize = int((float(img.size[1])*float(wpercent)))
+	img = img.resize((baseheight,hsize), PIL.Image.ANTIALIAS)
 
-	img.save(os.path.join(directory, 'resized-' + image))
+	#new_image = img.fromarray(image_data_new)
+
+        img.save(os.path.join(directory, 'resized-' + image))
 
 	print "Batch Processing complete"
 
