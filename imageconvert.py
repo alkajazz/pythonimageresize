97cae569064396135c50b2cf676780e2b625fb2f
Nathan <nathanbooth@Nathans-MacBook-Air.local>
2015-09-02 13:52:32 -0500

    Added ability to select height or width

--- imageconvert.py
+++ imageconvert.py
@@ -3,45 +3,42 @@
 import os
 import sys
 import getopt
-import argparse
 import PIL
 from PIL import Image
-import numpy as np
-
-# command line functions
-
-
 
 def Command():
-    opts, args = getopt.getopt(sys.argv[1:], 'd:bw:bh:')
+    opts, args = getopt.getopt(sys.argv[1:], 'd:w:h:')
     directory = ''
     basewidth = -1
-    baseheight = 1200
+    baseheight = -1
     
     for opt, arg in opts:
 	if opt == '-d':
 	    directory = arg
-	elif opt == '-bw':
-	    width = int(arg)
-	elif opt == '-bh':
-	    height = int(arg)
-    
+	elif opt == '-w':
+	    basewidth = int(arg)
+	elif opt == '-h':
+	    baseheight = int(arg)
+
     for image in os.listdir(directory):
-	print('Resizing image' + image)
-        img = Image.open(os.path.join(directory, image))
-	#img.load()
-	#image_data = np.asarray(img)
 
-
-	wpercent = (baseheight/float(img.size[0]))
-	hsize = int((float(img.size[1])*float(wpercent)))
-	img = img.resize((baseheight,hsize), PIL.Image.ANTIALIAS)
-
-	#new_image = img.fromarray(image_data_new)
-
-        img.save(os.path.join(directory, 'resized-' + image))
-
-	print "Batch Processing complete"
+	if basewidth > -1:
+	    print('Resizing image' + image)
+	    img = Image.open(os.path.join(directory, image))
+	    wpercent = (basewidth/float(img.size[0]))
+	    hsize = int((float(img.size[1])*float(wpercent)))
+	    img = img.resize((basewidth,hsize), PIL.Image.ANTIALIAS)
+	    img.save(os.path.join(directory, 'resized-' + image))
+	    print "Batch Processing Complete"
+	
+	else:
+	    print('Resizing Image' + image)
+	    img = Image.open(os.path.join(directory, image))
+	    wpercent = (baseheight/float(img.size[0]))
+            hsize = int((float(img.size[1])*float(wpercent)))
+	    img = img.resize((baseheight,hsize), PIL.Image.ANTIALIAS)
+            img.save(os.path.join(directory, 'resized-' + image))
+	    print "Batch Processing complete"
 
 
 Command()
